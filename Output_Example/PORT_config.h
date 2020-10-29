b82cf9d6c356a3863cdbc9104f3f1e7ae270dc8e
waleed-adel <59749064+waleed-adel@users.noreply.github.com>
2020-10-29 09:12:51 +0200

    Second Commit
    added: 1- .ui file
                2- output example folder
                3-  removed final from file names

---
+++ Output_Example/PORT_config.h
@@ -0,0 +1,82 @@
+/************************************************/
+/*Author	:           				                  */
+/*Version	:   								                  */
+/*Date 		:           					                */
+/************************************************/
+
+/*Output = 1 .. Input = 0*/
+
+/*************PORTA*************/
+#define PORTA_PIN0_DIR          0
+#define PORTA_PIN0_INIT_VALUE   1
+#define PORTA_PIN1_DIR          0
+#define PORTA_PIN1_INIT_VALUE   1
+#define PORTA_PIN2_DIR          0
+#define PORTA_PIN2_INIT_VALUE   1
+#define PORTA_PIN3_DIR          0
+#define PORTA_PIN3_INIT_VALUE   1
+#define PORTA_PIN4_DIR          0
+#define PORTA_PIN4_INIT_VALUE   1
+#define PORTA_PIN5_DIR          0
+#define PORTA_PIN5_INIT_VALUE   1
+#define PORTA_PIN6_DIR          0
+#define PORTA_PIN6_INIT_VALUE   1
+#define PORTA_PIN7_DIR          0
+#define PORTA_PIN7_INIT_VALUE   1
+
+
+/*************PORTB*************/
+#define PORTB_PIN0_DIR          0
+#define PORTB_PIN0_INIT_VALUE   0
+#define PORTB_PIN1_DIR          0
+#define PORTB_PIN1_INIT_VALUE   0
+#define PORTB_PIN2_DIR          0
+#define PORTB_PIN2_INIT_VALUE   0
+#define PORTB_PIN3_DIR          0
+#define PORTB_PIN3_INIT_VALUE   0
+#define PORTB_PIN4_DIR          0
+#define PORTB_PIN4_INIT_VALUE   0
+#define PORTB_PIN5_DIR          0
+#define PORTB_PIN5_INIT_VALUE   0
+#define PORTB_PIN6_DIR          0
+#define PORTB_PIN6_INIT_VALUE   0
+#define PORTB_PIN7_DIR          0
+#define PORTB_PIN7_INIT_VALUE   0
+
+
+/*************PORTC*************/
+#define PORTC_PIN0_DIR          1
+#define PORTC_PIN0_INIT_VALUE   1
+#define PORTC_PIN1_DIR          1
+#define PORTC_PIN1_INIT_VALUE   1
+#define PORTC_PIN2_DIR          1
+#define PORTC_PIN2_INIT_VALUE   1
+#define PORTC_PIN3_DIR          1
+#define PORTC_PIN3_INIT_VALUE   1
+#define PORTC_PIN4_DIR          1
+#define PORTC_PIN4_INIT_VALUE   1
+#define PORTC_PIN5_DIR          1
+#define PORTC_PIN5_INIT_VALUE   1
+#define PORTC_PIN6_DIR          1
+#define PORTC_PIN6_INIT_VALUE   1
+#define PORTC_PIN7_DIR          1
+#define PORTC_PIN7_INIT_VALUE   1
+
+
+/*************PORTD*************/
+#define PORTD_PIN0_DIR          1
+#define PORTD_PIN0_INIT_VALUE   0
+#define PORTD_PIN1_DIR          1
+#define PORTD_PIN1_INIT_VALUE   0
+#define PORTD_PIN2_DIR          1
+#define PORTD_PIN2_INIT_VALUE   0
+#define PORTD_PIN3_DIR          1
+#define PORTD_PIN3_INIT_VALUE   0
+#define PORTD_PIN4_DIR          1
+#define PORTD_PIN4_INIT_VALUE   0
+#define PORTD_PIN5_DIR          1
+#define PORTD_PIN5_INIT_VALUE   0
+#define PORTD_PIN6_DIR          1
+#define PORTD_PIN6_INIT_VALUE   0
+#define PORTD_PIN7_DIR          1
+#define PORTD_PIN7_INIT_VALUE   0
